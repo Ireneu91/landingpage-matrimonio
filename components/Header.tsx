5b493fe15f6aea99f3978ec318e8a65a1dcc8980
Giancarmelo Pittalà <giancarmelo.pittala@gmail.com>
2026-04-19 19:45:19 +0200

    CHORE: fix shadcn component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,34 @@
-export function Header () {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        aaaa    
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export function Header() {
+  const navitems = [
+    {
+      name: "Home",
+      href: "/",
+    },
+  ];
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {navitems.map((item) => (
+          <NavigationMenuItem key={item.name}>
+            <NavigationMenuTrigger>{item.name}</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink href={item.href}>
+                {item.name}
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
